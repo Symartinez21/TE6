4b83692aaccf26d431d4bad37a7d53b3769cc282
Symartinez21 <sophiaymartinez@gmail.com>
2022-10-29 19:57:26 -0700

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,2 +1,7 @@
-for (if x = 1; x <100 ; x = x + 3)
-  console.log(x);
+for (var z = 1; z < 101; z ++) {
+  if (z % 15 == 0) console.log("Fizzbuzz");
+  else if (z % 3 == 0) console.log("Fizz");
+  else if (z % 5 == 0) console.log("buzz");
+  else console.log(z);
+ }
+}
